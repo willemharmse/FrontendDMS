d473bdca056fabbb9c6d9a142aaffce033afbb19
willemharmse <harmse688@gmail.com>
2025-02-03 14:15:37 +0200

    Remove unused components and styles: delete NotFound, Forbidden, and related CSS files, along with obsolete modal and table styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { isMobile } from 'react-device-detect';
 import Forbidden from './components/Forbidden';
 import NotFound from './components/NotFound';
 import FileInfo from './components/FileInfo';
@@ -9,23 +10,43 @@ import CreatePage from './components/CreatePage';
 import UserManagement from './components/UserManagement';
 import PreviewPage from './components/PreviewPage';
 import DeveloperPage from './components/DeveloperPage';
-import ForgotPage from './components/ForgotPassword';
+import LoginPageMobile from './components/Mobile/LoginPageMobile';
+import ForgotPassword from './components/ForgotPassword';
+import ForgotPasswordMobile from './components/Mobile/ForgotPasswordMobile';
+import MobileFileInfo from './components/Mobile/MobileFileInfo';
+import MobileHomePage from './components/Mobile/MobileHomePage';
+import BatchUpload from './components/BatchUpload';
+import PreviewWord from './components/PreviewWord';
+import VersionControlPage from './components/VersionControlPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="FrontendDMS/" element={<LoginPage />} />
-        <Route path="FrontendDMS/home" element={<HomePage />} />
-        <Route path="FrontendDMS/documentManage" element={<FileInfo />} />
-        <Route path="FrontendDMS/documentCreate" element={<CreatePage />} />
-        <Route path='FrontendDMS/upload' element={<UploadPage />} />
-        <Route path='FrontendDMS/userManagement' element={<UserManagement />} />
+        {/* Desktop Routes */}
+        <Route path="FrontendDMS/" element={isMobile ? <Navigate to="FrontendDMS/mobileLogin" /> : <LoginPage />} />
+        <Route path="FrontendDMS/home" element={isMobile ? <Navigate to="FrontendDMS/mobileHome" /> : <HomePage />} />
+        <Route path="FrontendDMS/documentManage" element={isMobile ? <Navigate to="FrontendDMS/mobileFI" /> : <FileInfo />} />
+        <Route path="FrontendDMS/documentCreate" element={isMobile ? <Navigate to="FrontendDMS/mobileHome" /> : <CreatePage />} />
+        <Route path='FrontendDMS/upload' element={isMobile ? <Navigate to="FrontendDMS/mobileHome" /> : <UploadPage />} />
+        <Route path='FrontendDMS/userManagement' element={isMobile ? <Navigate to="FrontendDMS/mobileHome" /> : <UserManagement />} />
         <Route path='FrontendDMS/403' element={<Forbidden />} />
-        <Route path="FrontendDMS/preview/:fileId" element={<PreviewPage />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="FrontendDMS/preview/:fileId" element={isMobile ? <Navigate to="FrontendDMS/mobileHome" /> : <PreviewPage />} />
         <Route path="FrontendDMS/repair" element={<DeveloperPage />} />
-        <Route path="FrontendDMS/forgot" element={<ForgotPage />} />
+        <Route path='FrontendDMS/forgot' element={isMobile ? <Navigate to="FrontendDMS/mobileForgot" /> : <ForgotPassword />} />
+        <Route path='FrontendDMS/updateFile' element={isMobile ? <Navigate to="FrontendDMS/mobileHome" /> : <VersionControlPage />} />
+
+        {/* Mobile Routes */}
+        <Route path='FrontendDMS/mobileLogin' element={!isMobile ? <Navigate to="FrontendDMS/" /> : <LoginPageMobile />} />
+        <Route path='FrontendDMS/mobileForgot' element={!isMobile ? <Navigate to="FrontendDMS/" /> : <ForgotPasswordMobile />} />
+        <Route path='FrontendDMS/mobileFI' element={!isMobile ? <Navigate to="FrontendDMS/" /> : <MobileFileInfo />} />
+        <Route path='FrontendDMS/mobileHome' element={!isMobile ? <Navigate to="FrontendDMS/" /> : <MobileHomePage />} />
+
+        {/* Batch Upload (Accessible from any device) */}
+        <Route path='FrontendDMS/batchUpload' element={<BatchUpload />} />
+
+        {/* Not Found Page */}
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
   );
