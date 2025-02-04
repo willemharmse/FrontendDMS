aa634285a135ae34f5e5fa790aa7069468fd6510
willemharmse <harmse688@gmail.com>
2025-02-04 14:32:06 +0200

    Fix label formatting in AbbreviationPopup component

--- src/components/ValueChanges/AbbreviationPopup.jsx
+++ src/components/ValueChanges/AbbreviationPopup.jsx
@@ -57,7 +57,7 @@ const AbbreviationPopup = ({ isOpen, onClose }) => {
                         />
                     </div>
                     <div className="abbr-input-group">
-                        <label>Description::</label>
+                        <label>Description:</label>
                         <input
                             type="text"
                             value={meaning}
